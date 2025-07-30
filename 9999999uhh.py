44f0487ec2b8047bf19116d85902bbf6ca7f5b3e
Scray Dev <152033952+madminjo@users.noreply.github.com>
2025-07-30 13:42:03 +0600

    Update 9999999uhh.py

--- 9999999uhh.py
+++ 9999999uhh.py
@@ -16,7 +16,7 @@ ALLOWED_GROUP_IDS = [-1002194959049]
 # 🔗 API-ссылки
 BAN_API = "https://scromnyi.vercel.app/region/ban-info?uid={uid}"
 LIKE_API_URL = "https://likes-scromnyi.vercel.app/like"
-LIKE_API_KEY = "7RK2ewDdhM2EjZDRfccAVC4x1vvbpo3H"
+LIKE_API_KEY = "ScromnyiDev"
 
 def timestamp_to_date(timestamp):
     try:
